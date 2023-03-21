8f89aefa711eace95722a712e920cf90e09d6a7a
not hehe <92992296+Plearm@users.noreply.github.com>
2023-03-21 08:17:53 +0300

    Update index.php

--- 1/3/PHP/index.php
+++ 1/3/PHP/index.php
@@ -42,9 +42,9 @@ if ($errors) {
 
 // Сохранение в базу данных.
 
-$user = 'db'; // Заменить на ваш логин uXXXXX
-$pass = '123'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=test', $user, $pass,
+$user = 'u52830'; // Заменить на ваш логин uXXXXX
+$pass = '7841698'; // Заменить на пароль, такой же, как от SSH
+$db = new PDO('mysql:host=localhost;dbname=u52830', $user, $pass,
   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
 
 // Подготовленный запрос. Не именованные метки.
