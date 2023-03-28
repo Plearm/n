30da72e2f9856efed7aa87252e34ddebd68be1b6
not hehe <92992296+Plearm@users.noreply.github.com>
2023-03-28 08:57:11 +0300

    Update form.php

--- 1/3/PHP/form.php
+++ 1/3/PHP/form.php
@@ -1,15 +1,4 @@
-<html lang="en">
-<head>
-  <meta charset='utf-8'/>
-  <link rel="stylesheet" href="style.css" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'/>
-</head>
-<body>
-<div class="form-wrapper">
-<div class="form-layer">
-<h1 class ="titles" id="linktitle"> Form </h1>
-  <form action ="backend/" method = "POST">
+<form action ="" method = "POST">
 	<label>
 			Name:<br />
 			<input name="field-name-1" />
@@ -64,7 +53,3 @@
 	  Send: <br/>
 	  <input type="submit" value="Sending" />
   </form>
-</div>
-</div>
-</body>
-</html>
