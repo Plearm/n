99c41b976785576079f314eb3378aab883cab542
not hehe <92992296+Plearm@users.noreply.github.com>
2023-05-23 00:25:39 +0300

    Update form.php

--- 1/4/form.php
+++ 1/4/form.php
@@ -4,7 +4,6 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <title></title>
-    <link rel="stylesheet" href="Style.css">
     <style>
       .error {
         border: 5px solid red;
