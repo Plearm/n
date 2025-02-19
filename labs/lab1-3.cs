d0564d61bfea9e158705411a246fdda9211da15b
not hehe <92992296+Plearm@users.noreply.github.com>
2025-02-19 12:40:01 +0300

    Create lab1-3.cs
    Класс Client с инкапсуляцией и выводом

---
+++ labs/lab1-3.cs
@@ -0,0 +1,58 @@
+using System;
+
+public class Client
+{
+    // Приватные поля
+    private string _name;
+    private string _ownershipType;
+    private string _address;
+    private string _phone;
+    private string _contactPerson;
+
+    // Конструктор
+    public Client(string name, string ownershipType, string address, string phone, string contactPerson)
+    {
+        _name = name;
+        _ownershipType = ownershipType;
+        _address = address;
+        _phone = phone;
+        _contactPerson = contactPerson;
+    }
+
+    // Свойства (геттеры и сеттеры)
+    public string Name 
+    {
+        get => _name;
+        set => _name = value;
+    }
+
+    public string OwnershipType
+    {
+        get => _ownershipType;
+        set => _ownershipType = value;
+    }
+
+    public string Address
+    {
+        get => _address;
+        set => _address = value;
+    }
+
+    public string Phone
+    {
+        get => _phone;
+        set => _phone = value;
+    }
+
+    public string ContactPerson
+    {
+        get => _contactPerson;
+        set => _contactPerson = value;
+    }
+
+    // Метод для отображения полной информации о клиенте
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Компания: {Name}\nФорма собственности: {OwnershipType}\nАдрес: {Address}\nТелефон: {Phone}\nКонтактное лицо: {ContactPerson}");
+    }
+}
