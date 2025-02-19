46a23206798e5589a838096cb6cf58498b2e0a7c
not hehe <92992296+Plearm@users.noreply.github.com>
2025-02-19 16:39:27 +0300

    Update labs1-7.cs

--- labs/labs1-7.cs
+++ labs/labs1-7.cs
@@ -2,3 +2,6 @@ public void DisplayShortInfo()
 {
     Console.WriteLine($"Компания: {Name}, Телефон: {Phone}, Контактное лицо: {ContactPerson}");
 }
+
+
+DisplayShortInfo заменить на метод который выводит на экран и почитать по нему информацию
