2c208592402327665be56a624620ae88e110cfd9
not hehe <92992296+Plearm@users.noreply.github.com>
2025-02-28 09:23:26 +0300

    Update labs1-7.cs
    Обновление метода вывода

--- labs/labs1-7.cs
+++ labs/labs1-7.cs
@@ -5,3 +5,10 @@ public void DisplayShortInfo()
 
 
 DisplayShortInfo заменить на метод который выводит на экран и почитать по нему информацию
+
+
+
+public override string ToString()
+{
+    return $"Компания: {Name}, Телефон: {Phone}, Контактное лицо: {ContactPerson}";
+}
